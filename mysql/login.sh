b1ad3efd966215704d460e77c41864e618f4effd
videobase <videobase@tencent.com>
2021-12-11 17:21:00 +0800

    feat: mysql support remote-login

--- mysql/login.sh
+++ mysql/login.sh
@@ -1,2 +1,5 @@
 docker exec -it mysql-test bash
 # mysql -p
+
+## 外部client登录时需要使用
+# alter user 'root'@'%' identified with mysql_native_password by 'helloworld';
---
+++ rabbitmq/create.sh
@@ -0,0 +1,1 @@
+docker run -itd --name rabbitmq -p 5672:5672 rabbitmq
