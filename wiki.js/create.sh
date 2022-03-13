83af3ae0fd1511e9a9438f0a8ae259e8936d91d4
gelcoguo <gelcoguo@tencent.com>
2022-03-13 20:13:05 +0800

    feat: add jager && update wiki

--- wiki.js/create.sh
+++ wiki.js/create.sh
@@ -1,1 +1,15 @@
-docker run -d -p 8080:3000 --name wiki --restart unless-stopped -e "DB_TYPE=mysql" -e "DB_HOST=127.0.0.1" -e "DB_PORT=3306" -e "DB_USER=root" -e "DB_PASS=helloworld" -e "DB_NAME=wiki" ghcr.io/requarks/wiki:2
+_dbname="mysql-test"
+_dbpass="helloworld"
+_dbport="3306"
+
+_dbaddr=`docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $_dbname`
+
+if [ ! -z "$_dbaddr" -a "$_dbaddr" != " " ]
+then
+    echo "db container address $_dbaddr"
+    docker run -d -p 8080:3000 --name wiki --restart unless-stopped -e "DB_TYPE=mysql" -e "DB_HOST=$_dbaddr" -e "DB_PORT=3306" -e "DB_USER=root" -e "DB_PASS=helloworld" -e "DB_NAME=wiki" ghcr.io/requarks/wiki:2
+else
+    echo "db container not found!"
+    exit 1;
+fi
+
